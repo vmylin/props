d98eee21961d90b2af8ab3dc93cfd68519c02ce9
Victor <vlinmy06@gmail.com>
2020-02-06 21:24:24 -0800

    add different ways to render content

--- src/student.js
+++ src/student.js
@@ -1,19 +1,62 @@
-import React from "react"
+// import React from "react"
+
+// export function Student(props) {
+//     // first add a parameter to function component
+//     // parameter can be anything (but convention is to name props)
+//     // to receive value, it has to be props.(value) | it's like jQuery API
+//     return(
+//         <div>
+//             <h1>{props.name}</h1>
+//             <ul>
+//             <li>{props.physics}</li>
+//             <li>{props.chemistry}</li>
+//             <li>{props.biology}</li>
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export default Student;
+//*************** */
+
+// receive data in class component
+// use "this" keyword as shown
+
+// import React, {Component} from "react";
+
+// class Student extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.name}</h1>
+//                 <ul>
+//                     <li>{this.props.physics}</li>
+//                     <li>{this.props.chemistry}</li>
+//                     <li>{this.props.biology}</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Student;
+
+//========================
+
+// COMBINING JS & PROPS TO RENDER CONTENT
+
+// component can use JS if/else statement 
+// render different content based on different conditions
+
+import React from "react";
 
 export function Student(props) {
-    // first add a parameter to function component
-    // parameter can be anything (but convention is to name props)
-    // to receive value, it has to be props.(value) | it's like jQuery API
-    return(
-        <div>
-            <h1>{props.name}</h1>
-            <ul>
-            <li>{props.physics}</li>
-            <li>{props.chemistry}</li>
-            <li>{props.biology}</li>
-            </ul>
-        </div>
-    );
+    if(props.physics > 60 && props.chemistry > 60 && props.biology > 60) {
+        return <h2>Grade A</h2>;
+        
+    } else {
+        return <h2>Grade B</h2>
+    }
 }
 
 export default Student;
